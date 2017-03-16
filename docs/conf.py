67d9be30418acd8bbef66ad528d9ed93ca05305c
Yuta Segawa <kawalab.14.segawa@gmail.com>
2017-03-16 22:24:50 +0900

    Refreshed.

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'sgw_utils'
 copyright = u'2017, Author'
-author = u'Author'
+author = u'Yuta Segawa'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
